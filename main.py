c86e36ed99496e91c2c062eff6bb197f20193604
iiit2tech <iiit2tech@gmail.com>
2019-02-26 23:52:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+# Flask In-Prod at Openshift Basic
+
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Hello, World!"
+@app.route("/Redhat")
+def Redhat():
+    return "Hello, Redhat !"
+@app.route("/Opensource")
+def Opensource():
+    return "Hello, Opensource !"
+if __name__ == "__main__":
+    app.run(debug=True)
+
